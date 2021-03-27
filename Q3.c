bbe3bc2964f1fa9cab0894d920e3d9666a8476a1
NganarembaShija <76985647+NganarembaShija@users.noreply.github.com>
2021-03-27 10:44:00 +0530

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -7,8 +7,6 @@ struct node
     struct node *link;
 } * start;
 
-typedef struct node node;
-
 void createList(int);
 void display();
 void removeAndAdd();
@@ -16,7 +14,7 @@ void removeAndAdd();
 void main()
 {
     int size;
-    start = (node *)malloc(sizeof(node));
+    start = (struct node *)malloc(sizeof(struct node));
     printf("Enter the number of nodes: ");
     scanf("%d", &size);
     createList(size);
@@ -30,14 +28,14 @@ void main()
 void createList(int size)
 {
     int i;
-    node *traverse, *newNode;
+    struct node *traverse, *newNode;
     printf("Enter Node 1 data: ");
     scanf("%d", &start->data);
     traverse = start;
     start->link = NULL;
     for (i = 2; i <= size; i++)
     {
-        newNode = (node *)malloc(sizeof(node));
+        newNode = (struct node *)malloc(sizeof(struct node));
         printf("Enter Node %d data: ", i);
         scanf("%d", &newNode->data);
         newNode->link = NULL;
@@ -49,7 +47,7 @@ void createList(int size)
 
 void display()
 {
-    node *traverse;
+    struct node *traverse;
     int i = 0;
     printf("\n\n");
     for (traverse = start; traverse != NULL; traverse = traverse->link)
@@ -60,7 +58,7 @@ void display()
 
 void removeAndAdd()
 {
-    node *end = start, *newNode=(node*)malloc(sizeof(node));
+    struct node *end = start, *newNode=(struct node*)malloc(sizeof(struct node));
 
     //Finding last node 
     while (end->link != NULL)
